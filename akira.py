ca4d2ea89635835f36214672effc7edf4991040f
Tavares <134638696+T2vares01@users.noreply.github.com>
2025-10-10 23:18:18 -0300

    Update akira.py

--- akira.py
+++ akira.py
@@ -4,12 +4,17 @@ import pyttsx3 as p3
 engine = p3.init()
 engine.setProperty("rate", 200)
 engine.setProperty("volume", 1)
-
 rec = sr.Recognizer()
 # print(sr.Microphone().list_microphone_names())
-with sr.Microphone(5) as mic:
-    rec.adjust_for_ambient_noise(mic)
-    print("Pode falar que eu vou gravar")
-    audio = rec.listen(mic)
-    texto = rec.recognize_google(audio, language="pt-BR")
-    print(texto)
+while True:
+    with sr.Microphone(5) as mic:
+        rec.adjust_for_ambient_noise(mic)
+        print("Pode falar que eu vou gravar")
+        audio = rec.listen(mic)
+        try:
+            texto = rec.recognize_google(audio, language="pt-BR")
+            print(texto)
+        except sr.UnknownValueError:
+            print("Não entendi o que você disse.")
+        except sr.RequestError:
+            print("Erro ao conectar ao serviço de reconhecimento.")
